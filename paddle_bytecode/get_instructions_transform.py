b12c68b8c8993af4770160a272be81562f7a08c7
Tian Chao <tianchao20042008@163.com>
2023-04-06 12:31:30 +0000

    return .get_leaf_ast_nodes() to __call__()

--- paddle_bytecode/get_instructions_transform.py
+++ paddle_bytecode/get_instructions_transform.py
@@ -1,6 +1,6 @@
 from .get_leaf_ast_nodes_transform import GetLeafAstNodesTransform
 
 class GetInstructionsTransform:
-  def get_instructions(self, ast_node):
-    for ast_node in GetLeafAstNodesTransform().get_leaf_ast_nodes(ast_node):
+  def __call__(self, ast_node):
+    for ast_node in GetLeafAstNodesTransform()(ast_node):
       yield ast_node.instruction
--- paddle_bytecode/get_leaf_ast_nodes_transform.py
+++ paddle_bytecode/get_leaf_ast_nodes_transform.py
@@ -1,9 +1,9 @@
 class GetLeafAstNodesTransform:
 
-  def get_leaf_ast_nodes(self, ast_node):
+  def __call__(self, ast_node):
     children = list(ast_node.flat_children())
     if len(children) == 0:
       yield ast_node
     else:
       for child in children:
-        yield from self.get_leaf_ast_nodes(child)
+        yield from self(child)
